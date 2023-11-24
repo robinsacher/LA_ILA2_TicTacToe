5fdd9fbc5b8ac586169c124ffafb07229cad3961
robinsacher <110891559+robinsacher@users.noreply.github.com>
2023-11-24 08:50:25 +0100

    spielfunktionen

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Spielmodi auswählen
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("Anleitung").style.display = "none";
   document.getElementById("Computer").style.display = "none";
@@ -14,19 +15,28 @@ document.addEventListener("DOMContentLoaded", function () {
     .querySelector('a[href="#Computer"]')
     .addEventListener("click", function () {
       showView("TicTacToe");
+      initializeTicTacToe(true); // true, um gegen den Computer zu spielen
     });
 
   document
     .querySelector('a[href="#1vs1"]')
     .addEventListener("click", function () {
       showView("1vs1");
+      initialize1vs1();
     });
 
   document
     .querySelector('a[href="#TicTacToe"]')
     .addEventListener("click", function () {
       showView("TicTacToe");
+      initializeTicTacToe(false); 
     });
+
+  const startButton1vs1 = document.querySelector('#1vs1 .animierter-button');
+  startButton1vs1.addEventListener('click', start1vs1Game);
+
+  const startButtonTicTacToe = document.querySelector('#TicTacToe .animierter-button');
+  startButtonTicTacToe.addEventListener('click', startTicTacToeGame);
 });
 
 function showView(viewId) {
@@ -37,3 +47,114 @@ function showView(viewId) {
 
   document.getElementById(viewId).style.display = "block";
 }
+
+
+//Spielfunktionen
+document.addEventListener('DOMContentLoaded', function () {
+  const spielerX = 'X';
+  const spielerO = 'O';
+  let aktuellerSpieler = spielerX;
+  let einzelspielerModus = false;
+
+  const felder = document.querySelectorAll('.Feld');
+  const startButton = document.querySelector('.animierter-button');
+  startButton.addEventListener('click', startSpiel);
+
+  function startSpiel() {
+    einzelspielerModus = document.querySelector('#1vs1').style.display !== 'none';
+
+    felder.forEach(feld => {
+      feld.addEventListener('click', handleFeldClick, { once: true });
+    });
+
+    startButton.style.display = 'none';
+  }
+
+  function handleFeldClick(e) {
+    const ausgewaehltesFeld = e.target;
+
+    if (istFeldLeer(ausgewaehltesFeld)) {
+      setzeSymbol(ausgewaehltesFeld, aktuellerSpieler);
+
+      if (hatGewonnen()) {
+        alert(`Spieler ${aktuellerSpieler} hat gewonnen!`);
+        resetSpielbrett();
+      } else if (istUnentschieden()) {
+        alert('Unentschieden!');
+        resetSpielbrett();
+      } else {
+        wechsleSpieler();
+
+        if (einzelspielerModus && aktuellerSpieler === spielerO) {
+          setTimeout(computerZug, 500);
+        }
+      }
+    }
+  }
+
+  function istFeldLeer(feld) {
+    return feld.innerText === '';
+  }
+
+  function setzeSymbol(feld, spieler) {
+    feld.innerText = spieler;
+  }
+
+  function wechsleSpieler() {
+    aktuellerSpieler = aktuellerSpieler === spielerX ? spielerO : spielerX;
+  }
+
+  function hatGewonnen() {
+    const gewinnKombinationen = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    return gewinnKombinationen.some(kombination => {
+      const [a, b, c] = kombination;
+      return (
+        felder[a].innerText !== '' &&
+        felder[a].innerText === felder[b].innerText &&
+        felder[a].innerText === felder[c].innerText
+      );
+    });
+  }
+
+  function istUnentschieden() {
+    return Array.from(felder).every(feld => feld.innerText !== '');
+  }
+
+  function resetSpielbrett() {
+    felder.forEach(feld => {
+      feld.innerText = '';
+    });
+    aktuellerSpieler = spielerX;
+  }
+
+  function computerZug() {
+    const leereFelder = Array.from(felder).filter(feld => istFeldLeer(feld));
+
+    if (leereFelder.length > 0) {
+      const zufaelligesFeld = leereFelder[Math.floor(Math.random() * leereFelder.length)];
+      setzeSymbol(zufaelligesFeld, spielerO);
+
+      if (hatGewonnen()) {
+        alert(`Spieler ${spielerO} hat gewonnen!`);
+        resetSpielbrett();
+      } else if (istUnentschieden()) {
+        alert('Unentschieden!');
+        resetSpielbrett();
+      } else {
+        wechsleSpieler();
+      }
+    }
+  }
+});
+
+
